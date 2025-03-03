74d410549cd41bf1e99e09581f7879de063ecb30
April Hall <ari@arithefirst.com>
2025-03-03 09:36:03 -0500

    feat: Cell increment/decrement commands

--- src/main/java/com/arithefirst/Birp.java
+++ src/main/java/com/arithefirst/Birp.java
@@ -8,17 +8,18 @@ import java.util.ArrayList;
 public class Birp {
     public static void main(String[] args) {
         ArrayList<Integer> feed = new ArrayList<Integer>(0);
+        feed.add(0); // Inatlize the array to avoid Index Out of Bounds
         int pointer = 0;
 
         String source = readFile(args[0]);
-        for (int i = 0; i != source.length(); i++) {
-            // Print start value the pointer
+        for (int i = 1; i != source.length() + 1; i++) {
+            // Print start value the pointer (for debugging)
             System.out.print(String.format("Pointer: %d", pointer));
             try {
-                switch (source.charAt(i)) {
+                switch (source.charAt(i - 1)) {
                     case '>':
                         // Increment pointer, add 0 to feed if pointer is out of bounds
-                        if (i + 1 > source.length()) {
+                        if (i + 1 > feed.size()) {
                             feed.add(0);
                         }
                         pointer += 1;
@@ -31,6 +32,22 @@ public class Birp {
                             pointer -= 1;
                         }
                         break;
+                    case '+':
+                        // Increment value at pointer, reset to 0 if value is 255
+                        if (feed.get(pointer) == 255) {
+                            feed.set(pointer, 0);
+                        } else {
+                            feed.set(pointer, feed.get(pointer) + 1);
+                        }
+                        break;
+                    case '-':
+                        // Decrement value at pointer, reset to 255 if value is 0
+                        if (feed.get(pointer) == 0) {
+                            feed.set(pointer, 255);
+                        } else {
+                            feed.set(pointer, feed.get(pointer) - 1);
+                        }
+                        break;
                     default:
                         break;
                 }
@@ -38,9 +55,10 @@ public class Birp {
                 System.out.println(e.getMessage());
                 System.exit(1);
             }
-            // Print the end value of the pointer
-            System.out.println(String.format(" %d", pointer));
+            // Print the end value of the pointer (for debugging)
+            System.out.println(String.format(" %d (%d)", pointer, feed.get(pointer)));
         }
+        System.out.println(feed);
     }
 
     public static String readFile(String path) {
