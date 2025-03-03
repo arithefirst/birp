c76837d074283b971cb8d52299959d3fbfbd239e
April Hall <ari@arithefirst.com>
2025-03-03 01:22:18 -0500

    feat: Filereader functionality

--- src/main/java/com/arithefirst/Birp.java
+++ src/main/java/com/arithefirst/Birp.java
@@ -1,9 +1,30 @@
 package com.arithefirst;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Birp {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+        System.out.println(readFile(args[0]));
+    }
+
+    public static String readFile(String path) {
+        String data = "";
+        try {
+            File bfile = new File(path);
+            Scanner reader = new Scanner(bfile);
+            while (reader.hasNextLine()) {
+                String read = reader.nextLine();
+                data += read;
+            }
+            reader.close();
+            return data;
+        } catch (FileNotFoundException e) {
+            System.out.println(String.format("No such file exists: \"%s\".\n", path));
+            e.printStackTrace();
+            System.exit(2);
+            return "";
         }
     }
 }
