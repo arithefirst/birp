e2a893c2d1c18ef7ed5d1013da96168d103b5d4a
April Hall <ari@arithefirst.com>
2025-03-03 09:16:40 -0500

    feat: Data pointer increase and decrease commands

--- src/main/java/com/arithefirst/Birp.java
+++ src/main/java/com/arithefirst/Birp.java
@@ -3,10 +3,44 @@ package com.arithefirst;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Birp {
     public static void main(String[] args) {
-        System.out.println(readFile(args[0]));
+        ArrayList<Integer> feed = new ArrayList<Integer>(0);
+        int pointer = 0;
+
+        String source = readFile(args[0]);
+        for (int i = 0; i != source.length(); i++) {
+            // Print start value the pointer
+            System.out.print(String.format("Pointer: %d", pointer));
+            try {
+                switch (source.charAt(i)) {
+                    case '>':
+                        // Increment pointer, add 0 to feed if pointer is out of bounds
+                        if (i + 1 > source.length()) {
+                            feed.add(0);
+                        }
+                        pointer += 1;
+                        break;
+                    case '<':
+                        // Decrement pointer, throw error if pointer is out of bounds
+                        if (i - 1 < 0) {
+                            throw new IndexOutOfBoundsException("Memory error: -1");
+                        } else {
+                            pointer -= 1;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
+                System.exit(1);
+            }
+            // Print the end value of the pointer
+            System.out.println(String.format(" %d", pointer));
+        }
     }
 
     public static String readFile(String path) {
